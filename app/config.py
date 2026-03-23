2c9dd870311798b3d47b7cee3430e3c0dbe2fba9
Pedro Henriqe Belchior Alves <pedro@MacBookPro.bwrouter>
2026-03-23 09:35:31 -0300

    fix: add gemini and annthropic kyis in config.py

--- app/config.py
+++ app/config.py
@@ -5,3 +5,5 @@ load_dotenv()
 
 AI_PROVIDER = os.getenv("AI_PROVIDER")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
